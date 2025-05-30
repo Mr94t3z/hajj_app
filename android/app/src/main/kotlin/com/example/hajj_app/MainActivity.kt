da3b9d0b632062e309bf19769a238f6ec2017a8b
Muhamad Taopik <mr.94t3z@gmail.com>
2025-05-31 01:47:57 +0700

    Stop tracking android/gradle.properties

--- android/app/src/main/kotlin/com/example/hajj_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/hajj_app/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.hajj_app
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity() {
-}
+class MainActivity: FlutterActivity()
